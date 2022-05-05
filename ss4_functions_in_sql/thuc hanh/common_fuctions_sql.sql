851fb52c2cdc8a4c777f86a3c992ead6c0eb9021
phuongkophe <phuong230602@gmail.com>
2022-05-05 16:10:26 +0700

    up bt

---
+++ ss4_functions_in_sql/thuc hanh/common_fuctions_sql.sql
@@ -0,0 +1,18 @@
+use quan_ly_sinh_vien;
+select address, count(student_id) as 'Số lượng học viên'
+from student
+group by address;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name
+having avg(mark) > 15;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name
+having avg(mark) >= all (select avg(mark) from mark group by mark.student_id);
